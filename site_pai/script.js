e1b9cb2e3af472f7974f4f8cfa6b6c151d9be531
Anasutero <129601393+Anasutero@users.noreply.github.com>
2023-09-28 16:35:43 -0300

    projeto de site moveis planejados
    site trabalho do senai

---
+++ site_pai/script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+// slider
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+setInterval ( function(){
+    nextImage();
+},2000)
+function nextImage(){
+    count++;
+    if(count>3){
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
